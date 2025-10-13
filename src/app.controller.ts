51b8eb193bd6cf5f9618a3320968a9f08ea37731
mezoo_adel <mo3tz3adel@gmail.com>
2025-10-13 16:48:01 +0400

    add dotenv package and start with user module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  index(@Query('id') id: string, @Query('name') name: string) {
+    return { id, name };
+  }
+
+  @Patch(':app')
+  update(@Param('app') app: string, @Body() body: object) {
+    return { app, body };
   }
 }
